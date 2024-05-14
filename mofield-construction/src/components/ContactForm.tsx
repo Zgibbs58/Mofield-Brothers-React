f08ee10e7edcb44080da4ffdf3dbe38c78b9855a
Zach Gibbs <zacharywgibbs@gmail.com>
2024-05-14 14:31:47 -0500

    new deployment.

--- mofield-construction/src/components/ContactForm.tsx
+++ mofield-construction/src/components/ContactForm.tsx
@@ -221,11 +221,11 @@ const Contact = () => {
                       Send
                     </button>
                   </form>
-                  <div>
+                  <p>
                     This site is protected by reCAPTCHA and the Google
                     <a href="https://policies.google.com/privacy">Privacy Policy</a> and
                     <a href="https://policies.google.com/terms">Terms of Service</a> apply.
-                  </div>
+                  </p>
             </div>
         </motion.div>
     </>
