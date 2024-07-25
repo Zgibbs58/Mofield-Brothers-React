72ee27d7347877ca3148cf46f770af2cd3c5615e
Zach Gibbs <zacharywgibbs@gmail.com>
2024-07-25 15:17:06 -0500

    Added legal page.

---
+++ mofield-construction/src/app/legal/page.tsx
@@ -0,0 +1,181 @@
+export default function Page() {
+  return (
+    <div className="mx-8 lg:mx-32 my-32">
+      <h1 className="text-2xl mb-10">Terms and Conditions</h1>
+      <p className="mb-10">Last Updated: 7-25-2024</p>
+      <p className="mb-10">
+        Welcome to Mofield Brothers Contruction Co., LLC. By accessing or using our website, you agree to be bound by these Terms and Conditions and
+        our Privacy Policy. If you do not agree with these terms, please do not use our website.
+      </p>
+      <ol className="list-decimal list-inside">
+        <li>
+          Use of the Website
+          <ul className="list-disc list-inside ml-4">
+            <li>
+              You agree to use our website only for lawful purposes and in a way that does not infringe the rights of others or restrict or inhibit
+              their use and enjoyment of the website.
+            </li>
+            <li>
+              You are prohibited from using the website to post or transmit any material that is defamatory, obscene, fraudulent, or violates any law.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Intellectual Property
+          <ul className="list-disc list-inside ml-4">
+            <li>
+              All content on this website, including text, graphics, logos, images, and software, is the property of Mofield Brothers Contruction Co.,
+              LLC or its content suppliers and is protected by intellectual property laws.
+            </li>
+            <li>
+              You may not reproduce, distribute, modify, or create derivative works from any content on this website without our prior written
+              consent.
+            </li>
+          </ul>
+        </li>
+        <li>
+          User-Generated Content
+          <ul className="list-disc list-inside ml-4">
+            <li>
+              If you submit content to our website, such as comments or project details, you grant us a non-exclusive, royalty-free, perpetual, and
+              worldwide license to use, reproduce, modify, and display such content.
+            </li>
+            <li>
+              You are solely responsible for the content you submit and must ensure it does not violate any laws or infringe the rights of others.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Disclaimers and Limitations of Liability
+          <ul className="list-disc list-inside ml-4">
+            <li>The information on our website is provided &quot;as is&quot; without warranties of any kind, either express or implied.</li>
+            <li>We do not warrant that the website will be uninterrupted, error-free, or free of viruses or other harmful components.</li>
+            <li>
+              To the fullest extent permitted by law, Mofield Brothers Contruction Co., LLC disclaims all liability for any loss or damage arising out
+              of or in connection with your use of the website.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Governing Law
+          <ul className="list-disc list-inside ml-4">
+            <li>These Terms and Conditions are governed by and construed in accordance with the laws of the State of Tennessee.</li>
+            <li>Any disputes arising out of or relating to these Terms and Conditions will be resolved exclusively in the courts of Tennessee.</li>
+          </ul>
+        </li>
+        <li>
+          Changes to the Terms
+          <ul className="list-disc list-inside ml-4">
+            <li>We reserve the right to update or modify these Terms and Conditions at any time without prior notice.</li>
+            <li>Your continued use of the website following any changes indicates your acceptance of the new Terms and Conditions.</li>
+          </ul>
+        </li>
+        <li>
+          Contact Information
+          <ul className="list-disc list-inside ml-4">
+            <li>If you have any questions or concerns about these Terms and Conditions, please contact us via the contact form.</li>
+          </ul>
+        </li>
+      </ol>
+      <h2 className="text-2xl my-10">Privacy Policy</h2>
+      <p className="mb-10">Last Updated: 7-25-2024</p>
+      <p className="mb-10">
+        Mofield Brothers Contruction Co., LLC is committed to protecting your privacy. This Privacy Policy explains how we collect, use, and disclose
+        your personal information when you use our website.
+      </p>
+      <ol className="list-decimal list-inside">
+        <li>
+          Information We Collect
+          <ul className="list-disc list-inside ml-4">
+            <li>
+              We collect personal information you provide to us through contact forms, including your name, phone number, email address, address,
+              city, zip code, and project details.
+            </li>
+            <li>
+              We use Google Analytics to collect information about your use of our website, such as your IP address, browser type, and pages visited.
+              Google Analytics uses cookies to collect this information.
+            </li>
+            <li>
+              We use Google Analytics to collect information about your use of our website, such as your IP address, browser type, and pages visited.
+              Google Analytics uses cookies to collect this information.
+            </li>
+            <li>
+              We use Google&apos;s reCAPTCHA to protect our site from spam and abuse. reCAPTCHA collects information about your interaction with the
+              site to determine whether you are a human or a bot.
+            </li>
+          </ul>
+        </li>
+        <li>
+          How We Use Your Information
+          <ul className="list-disc list-inside ml-4">
+            <li>
+              We use your personal information to respond to your inquiries, provide you with information about our services, and process your project
+              details.
+            </li>
+            <li>We use Google Analytics to analyze website traffic and improve our website&apos;s functionality and user experience.</li>
+            <li>We use reCAPTCHA to protect our website from spam and abuse.</li>
+          </ul>
+        </li>
+        <li>
+          Disclosure of Your Information
+          <ul className="list-disc list-inside ml-4">
+            <li>
+              We do not sell, trade, or otherwise transfer your personal information to outside parties except as described in this Privacy Policy.
+            </li>
+            <li>
+              We may share your personal information with trusted third parties who assist us in operating our website and conducting our business, as
+              long as those parties agree to keep this information confidential.
+            </li>
+            <li>We may also disclose your information when required by law or to protect our rights, property, or safety.</li>
+          </ul>
+        </li>
+        <li>
+          Cookies
+          <ul className="list-disc list-inside ml-4">
+            <li>
+              Our website uses cookies to enhance your browsing experience and collect information about how you use our site. Cookies are small files
+              that a site or its service provider transfers to your computer&apos;s hard drive through your web browser.
+            </li>
+            <li>
+              You can choose to disable cookies through your browser settings, but some features of our site may not function properly if you do.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Security
+          <ul className="list-disc list-inside ml-4">
+            <li>We take reasonable measures to protect your personal information from unauthorized access, use, or disclosure.</li>
+            <li>
+              However, no method of transmission over the internet or method of electronic storage is 100% secure, so we cannot guarantee absolute
+              security.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Your Rights
+          <ul className="list-disc list-inside ml-4">
+            <li>You have the right to access, update, or delete your personal information. To do so, please contact us via the contact form.</li>
+            <li>
+              You may also opt out of receiving marketing communications from us by following the unsubscribe instructions in those communications.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Changes to This Privacy Policy
+          <ul className="list-disc list-inside ml-4">
+            <li>
+              We may update this Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on our website.
+            </li>
+            <li>Your continued use of the website after any changes indicates your acceptance of the updated Privacy Policy.</li>
+          </ul>
+        </li>
+        <li>
+          Contact Information
+          <ul className="list-disc list-inside ml-4">
+            <li>If you have any questions or concerns about this Privacy Policy, please contact us via the contact form.</li>
+          </ul>
+        </li>
+      </ol>
+    </div>
+  );
+}
--- mofield-construction/src/components/Footer.jsx
+++ mofield-construction/src/components/Footer.jsx
@@ -5,7 +5,7 @@ import NavLinks from "./NavLinks";
 const Footer = () => {
   return (
     // <footer className="flex flex-col py-4 px-6 md:px-24 bg-yellow-950 bg-opacity-90 dark:bg-base-200 text-gray-200 gap-3">
-    <footer className="flex flex-col py-4 px-6 md:px-24 bg-yellow-900 bg-opacity-80 h-full w-full bg-gradient-to-br from-yellow-950 dark:from-base-200 dark:bg-base-300 text-gray-200 gap-3">
+    <footer className="flex flex-col pt-8 pb-4 px-6 md:px-24 bg-yellow-900 bg-opacity-80 h-full w-full bg-gradient-to-br from-yellow-950 dark:from-base-200 dark:bg-base-300 text-gray-200 gap-3">
       <div className="grid md:grid-cols-2 items-center">
         <div className="hidden md:flex justify-start">
           <Image className="hidden md:block" src="/images/mofieldLogoWhite.png" alt="Mofield Brothers Contruction Logo" width={300} height={100} />
@@ -18,11 +18,17 @@ const Footer = () => {
         <Image src="/images/mofieldLogoWhite.png" alt="Mofield Brothers Contruction Logo" width={200} height={100} />
       </div>
       <aside className="flex flex-col justify-center text-center items-center">
-        <p className="text-xs md:text-sm">© {new Date().getFullYear()} Mofield Brothers Contruction Co., LLC. All rights reserved.</p>
+        <p className="text-xs md:text-sm">©Mofield Brothers Contruction Co., LLC. 2023-{new Date().getFullYear()} All rights reserved.</p>
+        <p className="text-xs md:text-sm text-center">
+          Click here to view our{" "}
+          <Link className="underline" href="/legal">
+            Terms and Conditions as well as our Privacy Policy.
+          </Link>
+        </p>
         <p className="text-xs md:text-sm">
           Website crafted by{" "}
           <a className="text-yellow-300" href="https://www.zacharywgibbs.com/">
-            Zach Gibbs
+            Zach Gibbs Web Development
           </a>{" "}
           - Elevate your online presence with my design and development services.
         </p>
