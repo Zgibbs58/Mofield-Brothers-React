20d9479240cb55b5ece3354db271ff3c57aaa58e
Zach Gibbs <zacharywgibbs@gmail.com>
2024-08-02 09:40:03 -0500

    updated seo og info.

--- mofield-construction/src/app/layout.tsx
+++ mofield-construction/src/app/layout.tsx
@@ -4,15 +4,29 @@ import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import GoogleCaptchaWrapper from "@/components/GoogleCaptchaWrapper";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 
 const rokkitt = Heebo({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Mofield Brothers Construction Company, trusted Tennessee contractors",
-  description: "Mofield Brothers Construction Company is a trusted Tennessee contractor specializing in water and sewer lines, directional drilling, excavation, and hauling.",
+  title: "Mofield Brothers | Construction Contractors in Tennessee",
+  description:
+    "Mofield Brothers Construction Company is a trusted Tennessee contractor specializing in water and sewer lines, directional drilling, excavation, and hauling.",
   keywords: ["construction", "contractor", "Tennessee", "water and sewer", "directional drilling", "excavation", "hauling"],
   robots: "index, follow",
+  openGraph: {
+    title: "Mofield Brothers | Construction Contractors in Tennessee",
+    description:
+      "Mofield Brothers Construction Company is a trusted Tennessee contractor specializing in water and sewer lines, directional drilling, excavation, and hauling.",
+    images: [
+      {
+        url: "https://mofieldbrothers.com/images/mofieldScreen.png",
+        width: 800,
+        height: 600,
+        alt: "Home Page",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -26,12 +40,12 @@ export default function RootLayout({
         <link rel="icon" href="/images/mLogoBlack.png" />
       </head>
       <body className={`overflow-x-hidden ${rokkitt.className}`}>
-          <Header />
-            <GoogleCaptchaWrapper>
-              {children}
-              <Analytics />
-            </GoogleCaptchaWrapper>
-          <Footer />
+        <Header />
+        <GoogleCaptchaWrapper>
+          {children}
+          <Analytics />
+        </GoogleCaptchaWrapper>
+        <Footer />
       </body>
     </html>
   );
